d5f17a451b16d3014ed5c9db33e21320c3c665aa
subhashiniy <37328663+subhashiniy@users.noreply.github.com>
2018-03-24 12:35:44 +0530

    Add files via upload

---
+++ text.py
@@ -0,0 +1,31 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+import numpy as np
+from sklearn.pipeline import Pipeline
+
+#Loading the data set - training data.
+twenty_train = fetch_20newsgroups(subset='train', shuffle=True) 
+
+#twenty_train.target_names #prints all the categories
+
+
+
+# Building a pipeline:  Extracting features from text files, TF-IDF, Training Naive Bayes (NB) classifier on training data.
+text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB())])
+
+
+text_clf = text_clf.fit(twenty_train.data, twenty_train.target)
+
+#Loading the data set - test data
+twenty_test = fetch_20newsgroups(subset='test', shuffle=True)
+
+# Performance of NB Classifier
+predicted = text_clf.predict(twenty_test.data)
+
+print("The Accuracy is: ")
+print(np.mean(predicted == twenty_test.target))
+
+
+
---
+++ textss.py
@@ -0,0 +1,38 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+import numpy as np
+from sklearn.pipeline import Pipeline
+#Loading the data set - training data.
+twenty_train = fetch_20newsgroups(subset='train', shuffle=True) 
+
+
+# Machine Learning
+# Training Naive Bayes (NB) classifier on training data.
+text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB())])
+text_clf = text_clf.fit(twenty_train.data, twenty_train.target)
+twenty_test = fetch_20newsgroups(subset='test', shuffle=True)
+predicted = text_clf.predict(twenty_test.data)
+print("The accuracy before using preprocessing is: ")
+print(np.mean(predicted == twenty_test.target))
+
+#Remove stopwrods and stemming is done
+from nltk.stem.snowball import SnowballStemmer
+stemmer = SnowballStemmer("english", ignore_stopwords=True)
+
+class StemmedCountVectorizer(CountVectorizer):
+    def build_analyzer(self):
+        analyzer = super(StemmedCountVectorizer, self).build_analyzer()
+        return lambda doc: ([stemmer.stem(w) for w in analyzer(doc)])
+    
+stemmed_count_vect = StemmedCountVectorizer(stop_words='english')
+
+text_mnb_stemmed = Pipeline([('vect', stemmed_count_vect), ('tfidf', TfidfTransformer()), 
+                             ('mnb', MultinomialNB(fit_prior=False))])
+
+text_mnb_stemmed = text_mnb_stemmed.fit(twenty_train.data, twenty_train.target)
+
+predicted_mnb_stemmed = text_mnb_stemmed.predict(twenty_test.data)
+print(" The Accuracy after using snowball stemmer is:")
+print(np.mean(predicted_mnb_stemmed == twenty_test.target))
